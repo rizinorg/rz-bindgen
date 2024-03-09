b49c9df6e96e18edd77eb20f5f2de17f54736321
Khairul Azhar Kasmiran <kazarmy@gmail.com>
2024-03-09 20:58:30 +0800

    Support user install of swig python module

---
+++ py_install.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import os
+import shutil
+import sys
+import sysconfig
+
+def install_files(scheme):
+    for file in sys.argv[1:]:
+        if file.endswith(".py"):
+            dest_type = 'purelib'
+        elif file.endswith(".so"):
+            dest_type = 'platlib'
+        install_dir = sysconfig.get_path(dest_type, scheme)
+        shutil.copy(file, install_dir)
+        print(f"Installing {os.path.basename(file)} to {install_dir}")
+
+try:
+    install_files(sysconfig.get_default_scheme())
+except PermissionError:
+    print("Installing to user site-packages since normal site-packages is not writeable")
+    install_files(f"{os.name}_user")
