33de873ae48224d49c0f093222abb80e5ad32eae
Anton Kochkov <anton.kochkov@gmail.com>
2024-12-19 23:36:54 +0800

    ci: use macOS 14 as macOS 12 is deprecated

--- cibw_before_all.sh
+++ cibw_before_all.sh
@@ -6,21 +6,23 @@ set -ex
 # Install deps
 pip3 install meson ninja meson-python build
 
-if command -v apt; then
+if command -v brew; then
+    brew install --require-sha swig
+elif command -v apt; then
     set +e
     grep VERSION_CODENAME=stretch /etc/os-release >/dev/null 2>/dev/null
     if [[ $? == 0 ]] ; then
-	# Required because manylinux_2_24 uses Debian Stretch and the repos are not available anymore
+        # Required because manylinux_2_24 uses Debian Stretch and the repos are not available anymore
         echo "deb http://archive.debian.org/debian stretch main" > /etc/apt/sources.list
         echo "deb http://archive.debian.org/debian-security stretch/updates main" >> /etc/apt/sources.list
         echo "Acquire::Check-Valid-Until no;" > /etc/apt/apt.conf.d/99no-check-valid-until
     fi
     set -e
-    apt update && apt install --assume-yes libclang-7-dev clang-7 llvm-7
+    apt update && apt install --assume-yes libclang-7-dev clang-7 llvm-7 sudo
 elif command -v apk; then
-    apk update && apk add clang-dev
+    apk update && apk add clang-dev sudo
 elif command -v yum; then
-    yum -y install llvm-toolset-7.0 llvm-toolset-7.0-llvm-devel
+    yum -y install llvm-toolset-7.0 llvm-toolset-7.0-llvm-devel sudo
     # (CentOS 7) Uncomment following lines to see required paths in build log:
     #   yum -y install centos-release-scl
     #   source scl_source enable llvm-toolset-7.0 || true
@@ -30,11 +32,12 @@ pushd rizin
 
 if [[ "$OSTYPE" =~ msys* ]]; then
     meson setup --buildtype=release --prefix='c:/rizin' --vsenv build
+    meson install -C build
 else
     meson setup --buildtype=release --libdir=lib build
+    sudo meson install -C build
 fi
 
-meson install -C build
 popd
 
 if command -v apt; then
