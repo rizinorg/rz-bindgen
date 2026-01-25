01316cf7fd2b1d7dcbd324f8b2e2834e0dd4b1f1
Khairul Azhar Kasmiran <kazarmy@gmail.com>
2026-01-25 18:57:00 +0800

    Revert "Try `sudo \`which meson\``"
    This reverts commit ef3c7a228949546c1814d4266f3ebd306568d416.

--- cibw_before_all.sh
+++ cibw_before_all.sh
@@ -43,7 +43,7 @@ else
         export CFLAGS="-include linux/limits.h"
     fi
     meson setup --buildtype=release --libdir=lib build
-    sudo `which meson` install -C build
+    sudo meson install -C build
 fi
 
 popd
